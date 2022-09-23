391d4ac8e6c2c530d62d27fa04742f88ec94739f
Tshering Sherpa <sherpatshering783@gmail.com>
2022-09-23 09:04:33 +0545

    connected model view and templates of category

--- core/khoja/admin.py
+++ core/khoja/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Category, Page
 
-admin.site.register(Category)
-
-
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 admin.site.register(Page, PageAdmin)
+
+#To customize prepopulate slug field
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
--- core/khoja/views.py
+++ core/khoja/views.py
@@ -1,13 +1,27 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Category, Page
+
+
 def index(request):
-    context = {
-        'boldmessage': 'Hello World!!!'
-    }
+    categories = Category.objects.order_by('-likes')[:5]
+    context = {}
+    context['categories'] = categories
     return render(request, 'khoja/index.html', context)
 
 def about(request):
     return render(request, 'khoja/about.html')
+
+def show_category(request, category_name_slug):
+    context = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context['pages'] =  pages
+        context['category'] = category
+    except Category.DoesNotExist:
+        context['category'] = None
+        context['pages'] = None
+
+    return render(request, 'khoja/category.html', context)
