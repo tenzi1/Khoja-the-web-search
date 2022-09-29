fc32abf0620d69805d0bfb3eb0575ec2186536d6
Tshering Sherpa <sherpatshering783@gmail.com>
2022-09-29 18:34:42 +0545

    added bootstrap

--- core/khoja/views.py
+++ core/khoja/views.py
@@ -40,77 +40,70 @@ def visitor_cookie_handler(request):
 
 
 
-#User registration view
-def user_register(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('khoja:index'))
-    else:
-        form = UserForm()
-    return render(request, 'khoja/user_register.html', {'form': form })
-# Create User and User profile
-def register(request):
-    registered = False
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
+# User authentication using custom views
+# # Create User and User profile
+# def register(request):
+#     registered = False
+#     if request.method == 'POST':
+#         form = UserForm(request.POST)
+#         profile_form = UserProfileForm(request.POST)
 
-        if form.is_valid and profile_form.is_valid():
-            user = form.save()
-            user.set_password(user.password)
-            user.save()
+#         if form.is_valid and profile_form.is_valid():
+#             user = form.save()
+#             user.set_password(user.password)
+#             user.save()
 
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
-            registered = True
-            # return redirect(reverse('khoja:index'))
-        else:
-            print(form.errors, profile_form.errors)
-    else:
-        form = UserForm()
-        profile_form = UserProfileForm()
-    context = {
-            'form':form,
-            'profile_form':profile_form,
-            'registered':registered
-        }
-    return render(request, 'khoja/register.html', context)
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#             if 'picture' in request.FILES:
+#                 profile.picture = request.FILES['picture']
+#             profile.save()
+#             registered = True
+#             # return redirect(reverse('khoja:index'))
+#         else:
+#             print(form.errors, profile_form.errors)
+#     else:
+#         form = UserForm()
+#         profile_form = UserProfileForm()
+#     context = {
+#             'form':form,
+#             'profile_form':profile_form,
+#             'registered':registered
+#         }
+#     return render(request, 'khoja/register.html', context)
         
-# View for User Login
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+# # View for User Login
+# def user_login(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
     
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect(reverse('khoja:index'))
-            else:
-                return HttpResponse('Your account is disabled.')
-        else:
-            return HttpResponse('Invalid Credentials supplied !!!')
-    else:
-        return render(request, 'khoja/login.html')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 return redirect(reverse('khoja:index'))
+#             else:
+#                 return HttpResponse('Your account is disabled.')
+#         else:
+#             return HttpResponse('Invalid Credentials supplied !!!')
+#     else:
+#         return render(request, 'khoja/login.html')
 
-#View for logging out user
-@login_required
-def user_logout(request):
-    logout(request)
-    return redirect(reverse('khoja:index'))
+# #View for logging out user
+# @login_required
+# def user_logout(request):
+#     logout(request)
+#     return redirect(reverse('khoja:index'))
 
 
 
 def index(request):
     categories = Category.objects.order_by('-likes')[:5]
+    pages = Page.objects.order_by('-views')[:5]
     context = {}
     context['categories'] = categories
+    context['pages'] = pages
 
     #setting up test cookie
     request.session.set_test_cookie()
@@ -179,9 +172,9 @@ def add_page(request, category_name_slug):
                 page.views = 0
                 page.save()
 
-                return redirect(reverse('khoja:add_category', kwargs={'category_name_slug':category_name_slug}))
+                return redirect(reverse('khoja:add_category'))
         else:
-            print(form.erros)
+            print(form.errors)
     
     context = {
         'form': form,
